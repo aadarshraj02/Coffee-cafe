bec36195e8b454a4d91240fab4dd7ad1f9f24c83
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-30 09:57:48 +0530

    styled nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
+import Logo from "../assets/website/coffee_logo.png";
+
 const Navbar = () => {
   return (
     <div className="bg-gradient-to-r from-secondary to-secondary/90 text-white">
       <div className="container  ">
         <div className="flex">
           <div>
-            <a href="#">
-              <img src="" alt="" />
+            <a
+              href="#"
+              className="font-bold text-2xl sm:text-3xl flex justify-center items-center gap-2 tracking-wider font-cursive"
+            >
+              <img src={Logo} alt="" className="w-14" />
               Coffee Cafe
             </a>
           </div>
