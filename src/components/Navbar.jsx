6015a39758c7a8b8d6c65eae0746382a724c6d97
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-30 09:51:56 +0530

    added images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,14 @@ const Navbar = () => {
   return (
     <div className="bg-gradient-to-r from-secondary to-secondary/90 text-white">
       <div className="container  ">
-        <div className="flex"></div>
+        <div className="flex">
+          <div>
+            <a href="#">
+              <img src="" alt="" />
+              Coffee Cafe
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
