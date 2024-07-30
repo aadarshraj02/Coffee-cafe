cee69936d8afe85dbce985448ae96b71936b1c16
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-30 09:25:45 +0530

    initialized aos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import AOS from "aos";
+
 const App = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 700,
+      easing: "ease-in",
+      delay: 100,
+    });
+  });
+
   return <div></div>;
 };
 
