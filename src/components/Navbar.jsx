24c38692f77e932a5d7d2a8348c3269529e31a32
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-30 09:47:29 +0530

    added bg color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
-    <div>
-      <div className="container ">
+    <div className="bg-gradient-to-r from-secondary to-secondary/90 text-white">
+      <div className="container  ">
         <div className="flex"></div>
       </div>
     </div>
