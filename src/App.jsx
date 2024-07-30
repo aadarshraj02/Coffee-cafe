2b0e122ff85233ead1cc29824799b6704482cc2f
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-30 09:31:31 +0530

    imported slick

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const App = () => {
   useEffect(() => {
