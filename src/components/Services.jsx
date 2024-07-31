990a080dac1437ec76818f42dc84735630cfcd39
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-31 21:20:52 +0530

    wrapped in fragment

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -26,39 +26,42 @@ const Services = () => {
     },
   ];
   return (
-    <div className="py-10">
-      <div className="container">
-        <div className="text-center mb-20">
-          <h1 className="text-4xl font-bold font-cursive text-gray-800">
-            Best Coffee For You
-          </h1>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-14 md:gap-5 place-items-center">
-          {servicesData.map((data, index) => (
-            <div
-              key={index}
-              className="rounded-2xl bg-white hover:bg-primary hover:text-white shadow-xl duration-200 max-w-[300px] relative"
-              data-aos="fade-up"
-              data-aos-delay={data.aosDelay}
-            >
-              <div className="h-[130px]">
-                <img
-                  src={data.img}
-                  alt=""
-                  className="max-w-[200px] block mx-auto transform -translate-y-14 hover:scale-110 hover:rotate-6 duration-300"
-                />
+    <>
+      <span id="services"></span>
+      <div className="py-10">
+        <div className="container">
+          <div className="text-center mb-20">
+            <h1 className="text-4xl font-bold font-cursive text-gray-800">
+              Best Coffee For You
+            </h1>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-14 md:gap-5 place-items-center">
+            {servicesData.map((data, index) => (
+              <div
+                key={index}
+                className="rounded-2xl bg-white hover:bg-primary hover:text-white shadow-xl duration-200 max-w-[300px] relative"
+                data-aos="fade-up"
+                data-aos-delay={data.aosDelay}
+              >
+                <div className="h-[130px]">
+                  <img
+                    src={data.img}
+                    alt=""
+                    className="max-w-[200px] block mx-auto transform -translate-y-14 hover:scale-110 hover:rotate-6 duration-300"
+                  />
+                </div>
+                <div className="p-4 text-center ">
+                  <h1 className="text-xl font-bold">{data.name}</h1>
+                  <p className="text-gray-400  duration-200 text-sm hover:text-white line-clamp-2 ">
+                    {data.description}
+                  </p>
+                </div>
               </div>
-              <div className="p-4 text-center ">
-                <h1 className="text-xl font-bold">{data.name}</h1>
-                <p className="text-gray-400  duration-200 text-sm hover:text-white line-clamp-2 ">
-                  {data.description}
-                </p>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
