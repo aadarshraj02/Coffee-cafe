941af41b15083505ca15a6e124df4859bd8c3055
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-31 09:16:46 +0530

    styled nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import Logo from "../assets/website/coffee_logo.png";
+import { SiCoffeescript } from "react-icons/si";
 
 const Navbar = () => {
   const menus = [
@@ -21,7 +22,7 @@ const Navbar = () => {
   return (
     <div className="bg-gradient-to-r from-secondary to-secondary/90 text-white">
       <div className="container  ">
-        <div className="flex">
+        <div className="flex justify-between items-center gap-4">
           <div>
             <a
               href="#"
@@ -31,21 +32,21 @@ const Navbar = () => {
               Coffee Cafe
             </a>
           </div>
-          <div>
+          <div className="flex justify-between items-center gap-4">
             <ul className="hidden sm:flex items-center gap-4">
               {menus.map((menu, index) => (
                 <li key={index}>
                   <a
+                    className="inline-block text-xl py-4 px-4 text-white/70 hover:text-white duration-200"
                     href={menu.link}
-                    className="hover:text-secondary hover:underline"
                   >
                     {menu.name}
                   </a>
                 </li>
               ))}
             </ul>
-            <button>
-              Order
+            <button className="bg-primary/70 px-4 py-2 rounded-full hover:scale-105 duration-200 flex items-center gap-3">
+              Order <SiCoffeescript className="text-xl " />
             </button>
           </div>
         </div>
