27c1d10ca442d6626216bbb92e6c7d8e244b9123
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-31 21:51:10 +0530

    styled image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,10 +12,14 @@ const Banner = () => {
   return (
     <>
       <div style={bgImage}>
-        <div className="container">
+        <div className="container min-h-[550px] flex justify-center items-center py-12 sm:py-0 ">
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
             <div>
-              <img src="../../public/coffee-white.png" alt="" />
+              <img
+                className="max-w-[430px] w-full mx-auto spin drop-shadow-[10px_-10px_12px_rgba(0,0,0,1)]"
+                src="../../public/coffee-white.png"
+                alt=""
+              />
             </div>
             <div></div>
           </div>
