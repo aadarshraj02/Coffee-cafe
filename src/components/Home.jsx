3b099b51580686c1e809a349d649713515bf109a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-31 11:46:17 +0530

    added styling on div

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
-    <div>
-      <div className="container">
-        <div className="grid">
+    <div className="min-h-[550px] sm:min-h-[600px] bg-brandDark flex justify-center items-center text-white">
+      <div className="container pb-8 sm:pb-0">
+        <div className="grid grid-cols-1 sm:grid-cols-2">
           <div>
             <h1 className="text-5xl  sm:text-6xl lg:text-7xl font-bold">
               We serve the richest{" "}
