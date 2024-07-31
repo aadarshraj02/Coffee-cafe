8870d5d24006d3b08ead6598db7d41592aa5bcb0
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-31 11:33:50 +0530

    added h1 and styled

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,13 @@ const Home = () => {
     <div>
       <div className="container">
         <div className="grid">
-          <div></div>
+          <div>
+            <h1 className="text-5xl  sm:text-6xl lg:text-7xl font-bold">
+              We serve the richest{" "}
+              <span className="font-cursive text-primary">coffee</span> in the
+              city
+            </h1>
+          </div>
           <div></div>
         </div>
       </div>
