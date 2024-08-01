8ef205751b7ddcb3421b510fb57a1551a6cf7fbe
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-01 13:46:18 +0530

    added slider sttings

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -26,6 +26,43 @@ const testimonialData = [
   },
 ];
 const Testimonial = () => {
+  var settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    responsive: [
+      {
+        breakpoint: 10000,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <div>
       <div className="container mb-10 py-14 ">
@@ -35,7 +72,7 @@ const Testimonial = () => {
           </h1>
         </div>
         <div>
-          <Slider>
+          <Slider {...settings}>
             {testimonialData.map((data, index) => {
               return (
                 <div key={index} className="my-6">
