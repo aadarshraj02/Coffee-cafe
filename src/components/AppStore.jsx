4313d5164df0f73b98c9157e442d311e228e82d4
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-01 11:45:37 +0530

    added background style

--- src/components/AppStore.jsx
+++ src/components/AppStore.jsx
@@ -1,3 +1,12 @@
+const backgroundStyle = {
+  backgroundImage: "url('/website/coffee-beans-bg.png')",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  height: "100%",
+  width: "100%",
+};
+
 const AppStore = () => {
   return <div></div>;
 };
