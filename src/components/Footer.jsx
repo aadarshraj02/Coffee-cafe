9384bdefbfb3d2af43316178a169874f936f5251
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-01 14:36:04 +0530

    added fb,inst and linkedin icons from react iocns

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,7 @@
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 const Footer = () => {
   return <div></div>;
 };
