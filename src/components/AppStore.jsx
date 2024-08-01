0f95842bad3d53198f2ae5ef578611a317c13de7
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-01 12:00:33 +0530

    styled div in flex container

--- src/components/AppStore.jsx
+++ src/components/AppStore.jsx
@@ -13,11 +13,11 @@ const AppStore = () => {
       <div className="py-14" style={backgroundStyle}>
         <div className="container">
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center">
-            <div>
+            <div className="space-y-6 max-w-xl mx-auto">
               <h1 className="text-2xl text-center sm:text-left sm:text-4xl font-semibold pl-3 text-white/90">
                 Coffee Cafe is available for Android and IOS
               </h1>
-              <div>
+              <div className="flex flex-wrap justify-center sm:justify-start items-center">
                 <a href="#">
                   <img
                     src="/website/app_store.png"
