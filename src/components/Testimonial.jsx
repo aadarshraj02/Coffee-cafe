2b29bff4adf12e8dac9d9f2200130f747ee88f51
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-01 13:52:06 +0530

    styled h1,p and div

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -84,12 +84,15 @@ const Testimonial = () => {
                         alt=""
                       />
                     </div>
-                    <div>
-                      <div>
-                        <p>{data.text}</p>
-                        <h1>{data.name}</h1>
+                    <div className="flex flex-col items-center gap-4">
+                      <div className="space-y-3">
+                        <p className="text-xs text-gray-500 ">{data.text}</p>
+                        <h1 className="text-xl font-bold text-black/70 font-cursive">
+                          {data.name}
+                        </h1>
                       </div>
                     </div>
+                    <p></p>
                   </div>
                 </div>
               );
