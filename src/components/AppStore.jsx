6ce5f6250bd0db3f2edcdae16786623c9d07fda6
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-01 11:53:19 +0530

    added bg and updated text color to white

--- src/components/AppStore.jsx
+++ src/components/AppStore.jsx
@@ -10,11 +10,11 @@ const backgroundStyle = {
 const AppStore = () => {
   return (
     <>
-      <div className="py-14">
+      <div className="py-14" style={backgroundStyle}>
         <div className="container">
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center">
             <div>
-              <h1 className="text-2xl text-center sm:text-left sm:text-4xl font-semibold pl-3">
+              <h1 className="text-2xl text-center sm:text-left sm:text-4xl font-semibold pl-3 text-white/90">
                 Coffee Cafe is available for Android and IOS
               </h1>
             </div>
