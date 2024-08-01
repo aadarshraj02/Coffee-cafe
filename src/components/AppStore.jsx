fc7f148c1bb0f05167a73da59ce0f95f437e97bc
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-01 11:49:02 +0530

    wrapped in fragment and added container div

--- src/components/AppStore.jsx
+++ src/components/AppStore.jsx
@@ -8,7 +8,15 @@ const backgroundStyle = {
 };
 
 const AppStore = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <div className="container">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center"></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AppStore;
