10a55e5c43ee64634cf4a020c532522d5cf0c20c
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-01 12:25:14 +0530

    added header of testimonials

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,5 +1,15 @@
 const Testimonial = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="container mb-10 py-14 ">
+        <div className="text-center mb-10">
+          <h1 className="text-4xl font-bold font-cursive text-gray-800">
+            Testimonials
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonial;
