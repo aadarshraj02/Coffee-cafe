6eb18b79254e86b0c6bf122fb812ddf981d39257
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-01 11:49:58 +0530

    added a h1

--- src/components/AppStore.jsx
+++ src/components/AppStore.jsx
@@ -12,7 +12,11 @@ const AppStore = () => {
     <>
       <div>
         <div className="container">
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center"></div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center">
+            <div>
+              <h1>Coffee Cafe is available for Android and IOS</h1>
+            </div>
+          </div>
         </div>
       </div>
     </>
