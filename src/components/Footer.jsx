c21fe04e6116a7e3ceb7ff05e58746965173cadd
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-01 14:37:36 +0530

    added footer links and bg image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,34 @@ import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 
+const footerLinks = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
+
+const bgImage = {
+  backgroundImage: "url('../../public/website/coffee-footer.jpg')",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  minHeight: "400px",
+  width: "100%",
+};
+
 const Footer = () => {
   return <div></div>;
 };
